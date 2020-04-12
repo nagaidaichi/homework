9141c5bb73d6f052e13609525a2e12981fe89152
nagai <admblgld@gmail.com>
2020-04-12 18:04:36 +0900

    編集フォームを出現させて、確定するところまでを修正した

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ $(document).on('click','#add',function() {
   };
   // タスクを追加、編集ボタン追加
   $('#listItem').append(
-      '<div id="display"><p class="task"><input type="button" class="delButton" value="削除" >' + 
-        '<span class="editTask">' + $('#inputTodo').val()  + '</span>' + '<input type="button" class="editTask" value="編集">' + '</p></div>'
+      '<p class="task"><input type="button" class="delButton" value="削除" >' + 
+        '<span class="editTask">' + $('#inputTodo').val()  + '</span>' + '<input type="button" class="edit" value="編集">' + '</p>'
     );
   //入力内容を空白にする
   $('#inputTodo').val("");
@@ -15,52 +15,40 @@ $(document).on('click','#add',function() {
 
 // タスクを削除する
 $(document).on('click','.delButton',function() {
-    $(this).parent('p').fadeOut();
-  
+  $(this).parent('p').remove();
 });
+
 //全削除
 $(document).on('click','#reset',function(){
-  $('.task').fadeOut();
+  $('.task').remove();
   $('#inputTodo').focus();
 });
-
+var f = false;
 // 編集フォーム出現
-$(document).on('click','.editTask',function() {
-  if(!$(this).parent('p').attr('id')) {
-    $(this).parent('p').attr('id','on');
-    $('#on').parent('div').append('<div id="editForm"><input type="text" id="inputValue"><input type="button" id="confirm" value="確定"></div>');
-    $('#inputValue').focus();
-    var inputVal = $('#on').text();
-    $('#inputValue').val(inputVal);
-    $('#on').hide();
-
-    // カーソルが外れたら、確定
-    $('#inputValue').blur(function(){
-      if($('#inputValue').val().length === 0) {
-      $('#editForm').remove();
-      $('#on').show();
-      $('#on > span').text(inputVal);
-      $('p').removeAttr('id');
-      };
-      var value = $('#inputValue').val();
-      $('#editForm').remove();
-      $('#on').show();
-      $('#on > span').text(value);
-      $('p').removeAttr('id');
-    });
-  }
+$(document).on('click','.edit',function() {
+  var index = $('.task').index($(this).parent());
+  var todoContent = $(this).prev();
+  var inputVal = todoContent.text();
+  todoContent.hide();
+  var editForm = `<div id="editForm_${index}"><input type="text" id="inputValue_${index}"><input type="button" id="confirm" value="確定" onclick="submitTodo(event)"></div>`;
+  $(this).prev().prev().after(editForm);
+  var editFormInput = $(`#inputValue_${index}`);
+  $(editFormInput).focus();
+  $(editFormInput).val(inputVal);
 });
-// 確定ボタンを押すと上書きされる
-// $(document).on('click','#confirm' ,function(){
-//   if($('#inputValue').val().length === 0) {
-//     $('#editForm').remove();
-//       $('p').removeAttr('id');
-//       $('#on').show(inputVal);
-//   };
-//   var value = $('#inputValue').val();
-//   $('#editForm').remove();
-//   $('#on').show();
-//   $('#on > span').text(value);
-//   $('p').removeAttr('id');
-// });
+
+// 編集したTODOを確定させる
+function submitTodo(e) {
+  var target = e.target;
+  var value = $(target).prev().val();
+  if(value.length === 0) {
+    return;
+  };
+
+  var editTask = $(target).parent().next();
+  $(editTask).text(value);
+  $(target).parent().remove();
+  $(editTask).show();
+};
+
 

7956ab7143714853b36de7c11d7342f14b0339e1
nagai <admblgld@gmail.com>
2020-07-02 11:35:16 +0900

    コメントアウトを消しました

--- ajax.js
+++ ajax.js
@@ -39,25 +39,6 @@ function addTask(e) {
     $('#inputTodo').focus();
 }
 
-// function getStatus(id) {
-//   var status = [];
-//   $.ajax({
-//     url: 'http://localhost:8000/api/status',
-//     type: 'GET',
-//   })
-//   .done(function(data){
-//     status = data.filter(function(item){
-//       if(item.id == id){
-//         return item.status;
-//       }
-//     });
-//   })
-//   .fail(function(err){
-//     console.log(err);
-//   });
-//   return status;
-// 
-// }
 
 $(function(){
   $.ajax({
@@ -69,11 +50,9 @@ $(function(){
 
     data.map(function(item) {
       var taskContent = item.content;
-      // var st = item.status_id;
       var status = item.status.status;
       console.log(item);
-      console.log(status);//完了・未着手
-      // var status = getStatus(item.status_id);
+      console.log(status);
       var taskElement = 
         `<li class="task">
           ${taskContent}
